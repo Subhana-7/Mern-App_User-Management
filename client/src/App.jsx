3ccff7151f2dd12ad8c030157bc862ce5cc7d9b6
Subhana <subhanasn2005@gmail.com>
2025-03-02 20:24:11 +0530

    complete signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,24 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Signin from './pages/Signin';
-import Profile from './pages/Profile';
-import About from './pages/About';
-import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/SignUp";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Header from "./components/Header";
 
 const App = () => {
-  return(
+  return (
     <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/sign-up' element={<Signup/>}/>
-      <Route path='/login' element={<Signin/>}/>
-      <Route path='/profile' element={<Profile/>}/>
-      <Route path='/about' element={<About/>}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 };
 
 export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className='bg-cyan-300' >
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-5' >
-        <Link to='/' >
-        <h1 className='main-heading' >User Management</h1>
+    <div className="bg-cyan-300">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-5">
+        <Link to="/">
+          <h1 className="main-heading">User Management</h1>
         </Link>
-        <ul className='flex gap-4' >
-          <Link  to='/' >
+        <ul className="flex gap-4">
+          <Link to="/">
             <li>Home</li>
           </Link>
-          <Link  to='/about' >
+          <Link to="/about">
             <li>About</li>
           </Link>
-          <Link  to='/login' >
+          <Link to="/sign-in">
             <li>Sign in</li>
           </Link>
-          
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
