2bbf78015ed8489a580bd92d126f3899761db573
Subhana <subhanasn2005@gmail.com>
2025-03-03 07:49:47 +0530

    sign in frontend and backend implemented

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,84 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link ,useNavigate} from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 
-const Signin = () => {
+const SignIn = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        toast.error(data.message || "Something went wrong!");
+        return;
+      }
+      toast.success("user logged in!");
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+      toast.error(error.message || "Something went wrong!");
+    }
+  };
+
   return (
-    <div>
-      Signin
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-bold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-300 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-300 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>New user?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-500">Sign up</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
+      <ToastContainer position="top-right" />
     </div>
-  )
-}
+  );
+};
 
-export default Signin
+export default SignIn;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,47 +1,52 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link,useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 
 const Signup = () => {
-
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
-    setFormData({...formData, [e.target.id]:e.target.value})
-  }
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
+    try {
       setLoading(true);
       setError(false);
-      const res = await fetch('/api/auth/signup',{
-        method: 'POST',
-        headers:{
-          'Content-Type':'application/json',
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        body:JSON.stringify(formData)
+        body: JSON.stringify(formData),
       });
       const data = await res.json();
       setLoading(false);
-      if(data.success === false) {
+      if (data.success === false) {
         setError(true);
         toast.error(data.message || "Something went wrong!");
         return;
       }
       toast.success("Account created successfully!");
-    }catch(error){
+      navigate('/sign-in');
+    } catch (error) {
       setLoading(false);
       setError(true);
       toast.error(error.message || "Something went wrong!");
     }
-  }
+  };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-bold my-7">Signup</h1>
+      <h1 className="text-3xl text-center font-bold my-7">Sign up</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
@@ -64,7 +69,10 @@ const Signup = () => {
           className="bg-slate-300 p-3 rounded-lg"
           onChange={handleChange}
         />
-        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
           {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
@@ -74,7 +82,8 @@ const Signup = () => {
           <span className="text-blue-500">Sign in</span>
         </Link>
       </div>
-      <p className="text-red-700 mt-5" >{error && 'Something went wrong!'}</p>
+      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
+      <ToastContainer position="top-right" />
     </div>
   );
 };
